f09dd75693c5cb4bf812ee8b65afa377ddde27ed
alsoj <alsoj@naver.com>
2022-06-18 20:36:27 +0900

    change discount policy

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,11 +2,11 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
-import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
@@ -26,5 +26,6 @@ public class AppConfig {
 
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
     }
 }
