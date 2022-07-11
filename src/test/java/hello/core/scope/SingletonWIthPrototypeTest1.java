61944b1440b95827173a52f2d98b5409a9b1cb2d
alsoj <alsoj@naver.com>
2022-07-11 10:47:11 +0900

    add Provider

--- src/test/java/hello/core/scope/SingletonWIthPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWIthPrototypeTest1.java
@@ -3,12 +3,15 @@ package hello.core.scope;
 import ch.qos.logback.core.net.server.Client;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWIthPrototypeTest1 {
 
@@ -31,27 +34,23 @@ public class SingletonWIthPrototypeTest1 {
         int count1 = clientBean1.logic();
         Assertions.assertThat(count1).isEqualTo(1);
 
-
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
-
     }
 
     @Scope("prototype")
